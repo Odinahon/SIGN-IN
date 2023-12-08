0e9a7eb7fd4bb1c3ecb3fbb4afe5193ddb5a87e8
Odina Saydahmedova <osaydahmedova@launchcg.com>
2023-12-08 10:13:29 -0800

    First Commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ label, invalid, ...props }) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClasess = "w-full  px-3 py-2 leading-tight border rounded shadow";
+  if (invalid) {
+    labelClasses += " text-red-400";
+    inputClasess += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += " text-stone-300";
+    inputClasess += " text-gray-700 bg-stone-300";
+  }
+  return (
+    <p>
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasess} {...props} />
+    </p>
+  );
+}
